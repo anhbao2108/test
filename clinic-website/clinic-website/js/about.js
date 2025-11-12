3b439a6693a6aabe9a50c65e947d75a6c537fde9
anhbao2108 <n23dcpt003@student.ptithcm.edu.vn>
2025-11-12 17:00:00 +0700

    Delete clinic-website/clinic-website directory

--- clinic-website/clinic-website/js/about.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const doctorImg = document.querySelector('.doctor-img');
-  if (doctorImg) {
-    doctorImg.addEventListener('mouseover', () => doctorImg.style.transform = 'scale(1.06)');
-    doctorImg.addEventListener('mouseout', () => doctorImg.style.transform = 'scale(1)');
-  }
-});
--- clinic-website/clinic-website/js/main.js
+++
@@ -1,2 +0,0 @@
-// Main JS for all pages
-console.log('Main JS loaded');
